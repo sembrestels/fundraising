712cadfe0a1094506e12f2cf7f3e074801b6eb43
cd10012 <ced361@nyu.edu>
2019-07-07 02:05:53 +0200

    emit all contract addresses in deploy script

--- apps/kit-aragon-fundraising/scripts/deploy_dao.js
+++ apps/kit-aragon-fundraising/scripts/deploy_dao.js
@@ -1,6 +1,41 @@
+const namehash = require('eth-ens-namehash').hash
 const FundraisingKit = artifacts.require('FundraisingKit')
 const TokenMock = artifacts.require('TokenMock')
 const Controller = artifacts.require('AragonFundraisingController')
+const apps = ['finance', 'token-manager', 'vault', 'voting']
+const fundraisingApps = [
+  'fundraising-market-maker-bancor',
+  'fundraising-controller-aragon-fundraising',
+  'fundraising-module-tap',
+  'fundraising-module-pool',
+]
+
+// Ensure that these address are up to date according to the network
+// Defaults set here are for the local rpc
+const defaultOwner = process.env.OWNER || '0xb4124cEB3451635DAcedd11767f004d8a28c6eE7'
+const defaultENSAddress = process.env.ENS || '0x5f6f7e8cc7346a11ca2def8f827b7a0b612c56a1'
+const defaultHolders = [defaultOwner, '0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb']
+const defaultTokenSupply = 100
+
+const getInstalledApps = (receipt, daoInstance, appNames) => {
+  const daoAddress = getEventResult(receipt, daoInstance, 'dao')
+  if (daoAddress) {
+    let installedApps = {}
+    appNames.map(app => {
+      const appAddress = getEventResult(receipt, 'InstalledApp', app)
+      console.log("%s at: %s", app, appAddress)
+      installedApps[app] = appAddress
+    })
+    return { daoAddress, installedApps }
+  }
+}
+
+const getEventResult = (receipt, event, param) => {
+  if (event == 'InstalledApp') {
+    return receipt.logs.filter(l => l.event === 'InstalledApp' && l.args.appId === namehash(`${param}.aragonpm.eth`))[0].args.appProxy
+  }
+  return receipt.logs.filter(l => l.event == event)[0].args[param]
+}
 
 const getBuyOrderBatchId = receipt => {
   const event = receipt.logs.find(l => l.event === 'NewBuyOrder')
@@ -41,47 +76,49 @@ function increaseBlocks(blocks) {
 
 module.exports = async callback => {
   try {
-    const collateral1 = await TokenMock.new('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', 1000000000000000000)
-    const collateral2 = await TokenMock.new('0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', 1000000000000000000)
+    const fundraisingAddress = process.argv[6]
+    const network = process.argv[5]
 
-    const kit = await FundraisingKit.at(process.argv[6])
+    console.log(`Creating token instances with owner: ${defaultOwner} and supply: ${defaultTokenSupply}`)
+    const collateralMultisig = await TokenMock.new(defaultOwner, defaultTokenSupply)
+    const collateralBondedToken = await TokenMock.new(defaultOwner, defaultTokenSupply)
+    const kit = await FundraisingKit.at(fundraisingAddress)
 
+    const aragonId = 'fundraising' + Math.random()
     const receipt1 = await kit.newTokens('PRO', 'PROJECT')
     const receipt2 = await kit.newMultisigInstance(
-      'fundraising' + Math.random(),
-      ['0xb4124cEB3451635DAcedd11767f004d8a28c6eE7', '0x8401Eb5ff34cc943f096A32EF3d5113FEbE8D4Eb '],
-      2
+      aragonId,
+      defaultHolders,
+      defaultHolders.length
     )
-    const receipt3 = await kit.newFundraisingInstance(collateral1.address, collateral2.address)
-    const dao = receipt2.logs.filter(l => l.event == 'DeployMultisigInstance')[0].args.dao
+    const receipt3 = await kit.newFundraisingInstance(collateralMultisig.address, collateralBondedToken.address)
 
-    const controllerAddress = receipt3.logs.filter(
-      l => l.event === 'InstalledApp' && l.args.appId === '0xc48aa5a969ae8dcd08a42cd12dce8c3c494c4ba70eb0d3bef069f20469e04be1'
-    )[0].args.appProxy
+    const collateralMultisigAddress = receipt1.logs.filter(l => l.event == 'DeployToken')[0].args.token
+    const collateralBondedTokenAddress = receipt1.logs.filter(l => l.event == 'DeployToken')[1].args.token
 
-    const marketMakerAddress = receipt3.logs.filter(
-      l => l.event === 'InstalledApp' && l.args.appId === '0xd9f5fb0b31c6211801549521bac548c25cc14dce610e5e4c4ea2857ac4580d05'
-    )[0].args.appProxy
+    const { daoAddress: cacheAddress, installedApps: coreApps } = getInstalledApps(receipt2, 'DeployMultisigInstance', apps)
+    const { daoAddress, installedApps } = getInstalledApps(receipt3, 'DeployFundraisingInstance', fundraisingApps)
 
-    console.log(controllerAddress)
-    console.log(marketMakerAddress)
-
-    const controller = await Controller.at(controllerAddress)
-
-    console.log('OK')
-
-    await collateral1.approve(marketMakerAddress, 1000000000000000000)
-
-    const receipt4 = await controller.createBuyOrder(collateral1.address, 10000)
-
-    // const batchId = getBuyOrderBatchId(receipt4)
+    const controller = await Controller.at(installedApps[fundraisingApps[2]])
+    const receipt4 = await collateralBondedToken.approve(installedApps[fundraisingApps[0]], defaultTokenSupply)
 
     await increaseBlocks(1)
 
-    // const receipt = await curve.createBuyOrder(authorized, ETH, amount, { from: authorized, value: amount })
+    // TODO: These tx revert regardless of the size of the order, maybe we need to send ETH to the controller
+    //
+    // const receipt5 = await controller.createBuyOrder(collateralBondedToken.address, 1)
+    // const batchId = getBuyOrderBatchId(receipt4)
+    //
+    // const receipt6 = await curve.createBuyOrder(owner, ETH, 12, { from: defaultOwner, value: 12 })
+    // console.log(receipt6)
 
-    console.log('DAO deployed at ' + dao)
+    if (network === 'rpc') {
+      console.log('Start the Aragon client locally and go to:', daoAddress)
+    } else { // Rinkeby only
+      console.log('Visit your DAO at https://rinkeby.aragon.org/#/' + aragonId + '.aragonid.eth')
+    }
 
+    console.log('OK')
     callback()
   } catch (err) {
     console.log(err)
